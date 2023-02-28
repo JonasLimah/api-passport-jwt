233c75a48ec2060ed3e26e00bada9296e1d93c38
jonas Limah <jonaslimahpessoal@gmail.com>
2023-02-28 07:55:02 -0300

    .

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+/*
+criamos nossa pasta api-passport-jwt agora instalamos
+as dependencias necessarias[express,validator,sequelize,mysql2,dotenv,cors,passport,passport-jwt e jsonwebtoken]
+e seus types devido.
+
+proximo passo criar o servidor nosso ORM nossa api e por fim fazer as devidas validações dos usuarios,
+utilizando jsonwebtoken para gerarmos o token e validarmos com o passport.
+*/
